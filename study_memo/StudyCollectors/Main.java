be544b7c031acdbf4a404718ad845c7d5e8f1545
RonRole <50513757+RonRole@users.noreply.github.com>
2019-05-23 16:49:44 +0900

    Update and rename study_memo/StudyCollectors.java to study_memo/StudyCollectors/Main.java

--- study_memo/StudyCollectors/Main.java
+++ study_memo/StudyCollectors/Main.java
@@ -3,7 +3,7 @@ package main;
 import java.util.*;
 import java.util.stream.*;
 
-public class StudyCollectors {
+public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String[] inputWords = sc.nextLine().split(" ");
