0c887a5a328883081b2368fb0ad92bc5866703ee
RonRole <50513757+RonRole@users.noreply.github.com>
2019-05-23 16:47:19 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.util.*;
+import java.util.stream.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] inputWords = sc.nextLine().split(" ");
+		sc.close();
+
+		Map<String,List<String>> map = Arrays.stream(inputWords)
+												.collect(Collectors.groupingBy(word -> word, LinkedHashMap::new, Collectors.toList()));
+		map.forEach((key, value) -> System.out.println(key + " " + value.size()));
+	}
+}
