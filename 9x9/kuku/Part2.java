db88fb614df6cf0a3f785db29d179149217c9020
RonRole <50513757+RonRole@users.noreply.github.com>
2019-06-10 15:46:34 +0900

    Update Part2.java

--- 9x9/kuku/Part2.java
+++ 9x9/kuku/Part2.java
@@ -5,9 +5,10 @@ import java.util.stream.*;
 public class Part2 {
 	public static void main(String ...args) {
 		IntStream.rangeClosed(1, 9)
-					.mapToObj(num1 -> IntStream.rangeClosed(1, 9)
-												.mapToObj(num2 -> String.format("%02d", num1*num2))
-												.collect(Collectors.joining(" ")))
-					.forEach(System.out::println);
+			 .mapToObj(num1 -> IntStream.rangeClosed(1, 9)
+						    .mapToObj(num2 -> String.format("%02d", num1*num2))
+					 	    .collect(Collectors.joining(" "))
+				   )
+			.forEach(System.out::println);
 	}
 }
