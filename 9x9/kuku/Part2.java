4f6a60a512027e581e3fb0ceedf5ff73ca2e6473
RonRole <50513757+RonRole@users.noreply.github.com>
2019-06-10 15:48:26 +0900

    Update Part2.java

--- 9x9/kuku/Part2.java
+++ 9x9/kuku/Part2.java
@@ -2,6 +2,10 @@ package kuku;
 
 import java.util.stream.*;
 
+/**
+ * mapの中でさらにmapする
+ * forEachが1つになり、見やすくなる気がする
+ */
 public class Part2 {
 	public static void main(String ...args) {
 		IntStream.rangeClosed(1, 9)
